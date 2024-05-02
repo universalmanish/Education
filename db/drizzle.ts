c4d2c196ff18e613f0082a878db61e9b2b1945ec
Manish Chaudhary <126310496+universalmanish@users.noreply.github.com>
2024-05-02 08:37:27 +0000

    file change

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from "@/db/schema"
 
-const sql = neon("postgresql://origin_owner:Cwl2qj3ZRUmK@ep-wandering-boat-a1q0al4l.ap-southeast-1.aws.neon.tech/new?sslmode=require");
+const sql = neon("postgresql://Education_owner:0knUDgwEhb7i@ep-silent-dew-a12p8zb6.ap-southeast-1.aws.neon.tech/Education?sslmode=require");
 //@ts-ignore
 const db = drizzle(sql, {schema});
 
